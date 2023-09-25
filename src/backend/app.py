36d7983b9be555af002680fb809d7c5258e04ea9
dopric <opric.dragan@gmail.com>
2023-09-25 20:32:16 +0200

    router added

--- src/backend/app.py
+++ src/backend/app.py
@@ -1,40 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
+from api.todos.routes import router as todo_router
 
 app = FastAPI()
 
-todos = [{'id': 1, 'title': 'First Todo', 'description': 'This is the first todo', 'done': False}]
+router = APIRouter()
 
-@app.get("/")
-async def index():
-    return {"message": "Hello World"}
+app.include_router(todo_router, prefix='/api/v1/todos')
 
-@app.get("/api/todos")
-async def get_todos() -> list:
-    return todos
-
-@app.post("/api/todos")
-async def create_todos(todo: dict) -> dict:
-    todo['id'] = len(todos) + 1
-    todos.append(todo)
-    return todo
-
-@app.put("/api/todos/{id}")
-async def update_todos(id: int, todo: dict) -> dict:
-    for index, item in enumerate(todos):
-        if item['id'] == id:
-            todos[index] = todo
-    return todo
-
-@app.delete("/api/todos/{id}")
-async def delete_todos(id: int) -> dict:
-    for index, item in enumerate(todos):
-        if item['id'] == id:
-            todos.pop(index)
-    return {"message": "Todo deleted successfully"}
-
-@app.get("/api/todos/{id}")
-async def get_todo_by_id(id: int) -> dict:
-    for todo in todos:
-        if todo['id'] == id:
-            return todo
-    return {"message": "Todo not found"}
